b6e6bf48fa4bdde5420cb821c9397e5ab1e01ee7
aadame3311 <antonio.adame01@utrgv.edu>
2017-10-29 13:31:08 -0500

    added comments to movement script

--- SpecShooter/Assets/Scripts/Movement.cs
+++ SpecShooter/Assets/Scripts/Movement.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    //a few notes on how movement should work... 
+        //read key press.
+        //know which direction to move player based on key that was pressed. 
+        //work on learning how to rotate player based on key press. 
+        //set movement and rotation speed.
+    
+    //find either youtube videos or read the unity documentation... 
+    //recommended chanels: quill18creates, brackeys. 
+
+    //leggo boi. 
+
+
+
 	// Use this for initialization
 	void Start () {
 		
